146cd552db6ed51a45559a560c3399c53e15bb82
Nazarii Tsymbalista <nazarii.tsymbalista@gmail.com>
2020-02-03 10:35:26 +0200

    Update auto_update.sh
    pwd

--- hometask_2/auto_update.sh
+++ hometask_2/auto_update.sh
@@ -3,9 +3,9 @@
 #Write a script 'auto_update.sh' which will install updates for your Linux system every week.
 # The updated log file should be stored in /var/log/update-YYYYMMDD.log, where YYYY-year, MM-month, DD-day_of_month.
 # ***Extra task: configure log rotation to update-YYYYMMDD.log every month and keep the last 3 log files compressed.
-
+work_dir=$(pwd)
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 cat > cron.txt <<EOF 
-0 5 * * 1 /home/ntsymbal/scripts/auto_update.sh 2>&1 >> /var/log/update-`date +%Y%m%d`.log
+0 5 * * 1 $work_dir/auto_update.sh 2>&1 >> /var/log/update-`date +%Y%m%d`.log
 EOF
 crontab -l | cat - cron.txt >crontab.txt && crontab crontab.txt
